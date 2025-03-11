83029394478b535f64afc456dbcdea70cefbe14b
Wemi Moyela <wemimoyela@gmail.com>
2025-03-11 23:37:41 +0100

    added product model and store controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "store/index"
+  root "products#index"
   resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,4 +4,10 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "product attributes non-empty" do
+  
+    prod = Product.create title: "Title", description: "description", price: 10.0, image_url: "love.jpg"
+    assert prod.valid?
+  end
 end
